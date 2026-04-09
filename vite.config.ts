8aa4afa49119e036f6a8bcb3e00b84496aa1916a
MicroMamont <drundek@bk.ru>
2026-04-09 04:14:41 +0300

    импорт и обновление конфигов

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
-    port: 10999,
-    open: './src/index.html',
+    port: 10999
   },
+  resolve: {
+    tsconfigPaths: true
+  }
 });
